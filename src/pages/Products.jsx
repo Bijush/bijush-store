8a6f89d3bcf5536d91e1dc09e3d99a794f79fad9
bijush <38435589+Bijush@users.noreply.github.com>
2025-06-09 16:33:27 +0530

    Add files via upload

---
+++ src/pages/Products.jsx
@@ -0,0 +1,8 @@
+export default function Products() {
+  return (
+    <div className="p-4">
+      <h2 className="text-2xl font-bold">Our Products</h2>
+      <p>Coming soon... 🧥👕👟</p>
+    </div>
+  )
+}
